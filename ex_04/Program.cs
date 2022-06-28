a84e9b5ca1bd755d9196c265cfa4601155aa6465
Dante67yt <dante67yt@gmail.com>
2022-06-28 15:25:15 +0300

    ex_04 проверка написанного числа на двойное условие с АйПерсент (&&)

--- ex_04/Program.cs
+++ ex_04/Program.cs
@@ -1,7 +1,5 @@
 ﻿int a;
 Console.Write("Введите число ");
 a = Convert.ToInt32(Console.ReadLine());
-if (a % 7 == 0){
-    if (a % 23 == 0) Console.WriteLine("кратно");
-}
+if (a % 7 == 0 && a % 23 == 0) Console.WriteLine("кратно");
 else Console.Write("некратно");
