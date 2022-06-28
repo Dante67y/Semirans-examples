bee2713dfd56bb0c31d088216041c060975432ab
Dante67yt <dante67yt@gmail.com>
2022-06-28 15:13:34 +0300

    ex_03 добавил 'пользовательский интерфейс'

--- ex_03/Program.cs
+++ ex_03/Program.cs
@@ -1,9 +1,11 @@
 ﻿int a, b;
+Console.Write("Введите первое число ");
 a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
 b = Convert.ToInt32(Console.ReadLine());
-if (a%b == 0) Console.WriteLine("кратно");
+if (a % b == 0) Console.WriteLine("кратно");
 else
 {
     Console.Write("некратно, остаток ");
-    Console.WriteLine(a%b);
+    Console.WriteLine(a % b);
 }
