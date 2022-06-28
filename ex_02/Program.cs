a3d7cdc436874aaee10ae728635e6f42407a1862
Dante67yt <dante67yt@gmail.com>
2022-06-28 15:04:47 +0300

    add ex_02 берем трехзначное число, убираем среднюю цифру и пишем получившееся двухзначное число, ex_03 пишем 2 числа, проверяем их кратность и выводим остаток, если таковой есть

---
+++ ex_02/Program.cs
@@ -0,0 +1,5 @@
+﻿int n = new Random().Next(100, 999);
+Console.WriteLine(n);
+int n1 = n / 100;
+int n2 = n%10;
+Console.WriteLine(n1 * 10 + n2);
---
+++ ex_03/Program.cs
@@ -0,0 +1,11 @@
+﻿int a, b, x, y;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(a / b);
+x = Convert.ToInt32(a - b * (a / b));
+if (b * y == a) Console.WriteLine("кратно");
+else
+{
+    Console.Write("некратно, остаток ");
+    Console.WriteLine(x);
+}
