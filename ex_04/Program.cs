75eeb213735a3f25a6bad2de42fec33eba574580
Dante67yt <dante67yt@gmail.com>
2022-06-28 15:23:38 +0300

    ex_04 проверка написанного числа на двойное условие (моя версия относительно ex_03)

---
+++ ex_04/Program.cs
@@ -0,0 +1,7 @@
+﻿int a;
+Console.Write("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0){
+    if (a % 23 == 0) Console.WriteLine("кратно");
+}
+else Console.Write("некратно");
